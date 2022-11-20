372251dde87e82000456a80dd1754233762c3d92
CLAUDIU-GABRIEL MATEI <claudiu.gabriel.matei@stud.ubbcluj.ro>
2022-11-20 23:19:20 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Matei_Claudiu_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Expenses> Expenses { get; set; }
+    }
+}
---
+++ Models/Expenses.cs
@@ -0,0 +1,13 @@
+﻿namespace Matei_Claudiu_Lab6.Models
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
